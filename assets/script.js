7d2d319a6bc5956871e776355106f5cda8e1e4b3
kalanimojica <mojica.kalani@gmail.com>
2021-10-04 15:23:26 -0700

    revised HTML and JS files with added API key

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,93 @@
-// For searching city 
-var searchFormEl = document.querySelector("#search-form");
-// For displaying city and result
-var cityEl = document.querySelector("#city");
-var resultContentEl = document.querySelector("#result-content"); 
-var tempEl = document.querySelector("#temp"); 
-var humidityEl = document.querySelector("#wind-speed"); 
-var uvEl = document.querySelector("#uv-index");
+var weatherApiKey = "1c72e10c1966cc56d5ada2f30bd1c3c6";
+var searchHistory = [];
+var form = $("#search-form");
+var searchInput = $("#citySearch");
+var resultsContainer = $("#results");
+var searchHistoryContainer = $("#search-history");
 
-// the user inputs a city and clicks on the search button 
-function handleSearchFormSubmit(event) { 
-    event.preventDefault(); 
+// init search history
+searchHistory = localStorage.getItem("search-history");
+if (searchHistory) {
+  searchHistory = JSON.parse(searchHistory);
+} else {
+  searchHistory = [];
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var query = searchInput.val().trim();
+  if (query) {
+    searchGiphy(query);
+    searchInput.val("");
+    addSearchToHistory(query);
+  }
+}
+
+// Accepts a query and fetches data from the giphy api.
+function searchCity(query) {
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" + 
+    weatherApiKey;
+    console.log(requestUrl)
     
-    var searchInputVal = document.querySelector("#search-input").value;
-    if (!searchInputVal) { 
-        console.error("You need to put a city"); 
-        return
-    }
-    
-    var queryString = 
-    document.location.search + searchInputVal; 
-    location.assign(queryString); 
-    console.log(queryString)
+  fetch(requestUrl)
+    .then(function (response) {
+        console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      resultsContainer.empty();
+      for (var i = 0; i < data.data.length; i++) {
+        displayGiphy(data.data[i]);
+      }
+    });
 }
 
 
-function getParams() { 
-    var searchParamsArr = document.location.search.split('&');
-    var query = searchParamsArr[0].split('=').pop();
 
-    searchApi(query);
-    console.log(searchParamsArr)
+
+function displayGiphy(giphyResult) {
+  var imgUrl = giphyResult.images.downsized_large.url;
+  var title = giphyResult.title;
+
+  var img = $("<img>").attr({
+    src: imgUrl,
+    class: "img-fluid",
+    alt: title,
+  });
+  var col = $("<div>").addClass("col-12 col-lg-6 pb-4").append(img);
+
+  resultsContainer.append(col);
 }
 
+// Storing city search results 
+function displayButtons() {
+  searchHistoryContainer.empty();
+  // loop over searchHistory
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var button = $("<button>")
+      .attr({
+        type: "button",
+        class: "btn btn-outline-secondary btn-block btn-search",
+      })
+      .text(searchHistory[i]);
+    searchHistoryContainer.append(button);
+  }
+}
 
-// the input text is placed below the search bar 
-// The weather for the city will display the name of the city, the 
-// temp, wind speed, humidity, and UV index 
+function handleSearchClick() {
+  searchGiphy(this.textContent);
+}
 
-// Below the current city, will be the 5 day forecast 
+function addSearchToHistory(query) {
+  searchHistory.push(query);
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
+  displayButtons();
+}
 
-// the 5 day forecast will display the Date, temp, wind, and humidity.
+form.on("submit", handleFormSubmit);
+searchHistoryContainer.on("click", ".btn-search", handleSearchClick);
 
-// the users input will be stored in the local Storage. 
+displayButtons();

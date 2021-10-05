cee36e787235617899f72a09c1299792007fef3f
kalanimojica <mojica.kalani@gmail.com>
2021-10-04 22:13:04 -0700

    5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -42,14 +42,17 @@ function searchCity(query) {
      var lat = data[0].lat; 
      var long = data[0].lon;
      var requestWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + 
-     "&lon=" + long + "&units=imperial&exclude=hourly,daily&appid=" + weatherApiKey;
-      //resultsContainer.empty();
+     "&lon=" + long + "&units=imperial&exclude=hourly&appid=" + weatherApiKey;
+     
+
+      // Weather for the main card
       fetch(requestWeather)
       .then(function (response){ 
         return response.json();
       })
       .then(function(data){ 
           console.log(data)
+
         var temp = data.current.temp;
         var humidity = data.current.humidity;  
         var uvi = data.current.uvi;
@@ -64,50 +67,59 @@ function searchCity(query) {
         curentWindSpeedEl.textContent = "Wind-Speed: "+ windSpeed + "MPH";
         currentUVEl.textContent = "UV-Index: " + uvi; 
         
+
+
+        //Weather for 5 day forecast
+        var dailyWeather = data.daily[0]; 
+        var temp1 = document.querySelector("#temp1");
+        var humid1 = document.querySelector("#humid1");
+        var windSpeed1 = document.querySelector("#wind-speed1")
+        temp1.textContent = "Temperature: " + data.daily[0].temp.day;
+        humid1.textContent = "Humidity: " + data.daily[0].humidity;
+        windSpeed1.textContent = "Wind Speed: " + data.daily[0].wind_speed;
+
+
+        var dailyWeather2= data.daily[1];
+        var temp1 = document.querySelector("#temp2");
+        var humid1 = document.querySelector("#humid2");
+        var windSpeed1 = document.querySelector("#wind-speed2")
+        temp1.textContent = "Temperature: " + data.daily[1].temp.day;
+        humid1.textContent = "Humidity: " + data.daily[1].humidity;
+        windSpeed1.textContent = "Wind Speed: " + data.daily[1].wind_speed;
+
+        var dailyWeather3= data.daily[2];
+        var temp1 = document.querySelector("#temp3");
+        var humid1 = document.querySelector("#humid3");
+        var windSpeed1 = document.querySelector("#wind-speed3")
+        temp1.textContent = "Temperature: " + data.daily[2].temp.day;
+        humid1.textContent = "Humidity: " + data.daily[2].humidity;
+        windSpeed1.textContent = "Wind Speed: " + data.daily[2].wind_speed;
+
+        var dailyWeather4= data.daily[3]; 
+        var temp1 = document.querySelector("#temp4");
+        var humid1 = document.querySelector("#humid4");
+        var windSpeed1 = document.querySelector("#wind-speed4")
+        temp1.textContent = "Temperature: " + data.daily[3].temp.day;
+        humid1.textContent = "Humidity: " + data.daily[3].humidity;
+        windSpeed1.textContent = "Wind Speed: " + data.daily[3].wind_speed;
+
+        var dailyWeather5= data.daily[4];
+        var temp1 = document.querySelector("#temp5");
+        var humid1 = document.querySelector("#humid5");
+        var windSpeed1 = document.querySelector("#wind-speed5")
+        temp1.textContent = "Temperature: " + data.daily[4].temp.day;
+        humid1.textContent = "Humidity: " + data.daily[4].humidity;
+        windSpeed1.textContent = "Wind Speed: " + data.daily[4].wind_speed;
+
+        console.log(dailyWeather)
+        console.log(dailyWeather2)
+        console.log(dailyWeather3)
+        console.log(dailyWeather4)
+        console.log(dailyWeather5)
       })
     })
 } 
  
-    
-
-    
-
-
-function weekForecast(query) {
-    var requestUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      query +
-      "&appid=" + 
-      weatherApiKey;
-      
-      
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        
-        console.log(data)
-        var lat= data.coord.lat; 
-        var long= data.coord.lon;
-        var cityName = data.name;
-        var weatherIcon = data.weather[0].icon;
-        var temp = data.main.temp;
-        var humidity = data.main.humidity; 
-        var windSpeed = data.wind.speed;
-     
-        console.log(currentWeather)
-          //displayWeather(data, cityName);
-        displayWeather(data);
-        
-      })
-  }
-// Display current city weather
-// function displayWeather(result, city) { 
-//  currentTempEl.textContent = "Tempurature: " + data.weather.main.temp;
-
-// }
-
 
 // Storing city search results 
 function displayButtons() {
